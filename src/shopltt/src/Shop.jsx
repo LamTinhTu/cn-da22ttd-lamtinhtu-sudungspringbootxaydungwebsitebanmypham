6b0ea76930a95152a48aae83fca9399e17b5bad3
LamTinhTu <lttus231104@gmail.com>
2025-11-29 20:27:35 +0700

    commit UI

---
+++ src/shopltt/src/Shop.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import HeroSection from "./components/HeroSection/HeroSection"
+import NewArrivals from "./components/Sections/NewArrivals"
+import 'react-multi-carousel/lib/styles.css';
+import Category from "./components/Sections/Categories/Category";
+import content from './data/content.json';
+import Footer from "./components/Footer/Footer";
+
+const Shop = () => {
+    return (
+       <>
+       <HeroSection />
+       <NewArrivals />
+        {content?.categories && content?.categories?.map((item, index)=> <Category key={item?.title+index} {...item}/>)}
+       <Footer content={content?.footer}/>
+       </>
+    )
+}
+
+export default Shop
